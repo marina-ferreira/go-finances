27314afe9a754ad88c8c1556ea3864afd090ad88
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-14 19:27:12 -0300

    Dashboard balance

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-import income from 'assets/income.svg'
-import outcome from 'assets/outcome.svg'
-import total from 'assets/total.svg'
-
 import api from 'services/api'
-
-import Header from 'components/Header'
-
 import formatValue from 'utils/formatValue'
 import formatDate from 'utils/formatDate'
 
+import Header from 'components/Header'
+
+import income from 'assets/income.svg'
+import outcome from 'assets/outcome.svg'
+import total from 'assets/total.svg'
 import { Container, CardContainer, Card, TableContainer } from './styles'
 
 interface Transaction {
@@ -25,9 +23,9 @@ interface Transaction {
 }
 
 interface Balance {
-  income: string
-  outcome: string
-  total: string
+  income: number
+  outcome: number
+  total: number
 }
 
 const Dashboard: React.FC = () => {
@@ -55,21 +53,23 @@ const Dashboard: React.FC = () => {
               <p>Income</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{formatValue(balance.income)}</h1>
           </Card>
           <Card>
             <header>
               <p>Outcome</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              {formatValue(balance.outcome)}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{formatValue(balance.total)}</h1>
           </Card>
         </CardContainer>
 
