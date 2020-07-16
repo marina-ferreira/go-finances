02a8d1d21b911dedd4e9ee6d54d33ff113c3d494
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-16 08:54:33 -0300

    Updates format value helper

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,13 +1,8 @@
-const formatValue = (
-  value: number,
-  type: 'income' | 'outcome' = 'income'
-): string => {
-  const formattedValue = Intl.NumberFormat('en-US', {
+const formatValue = (value: number): string => {
+  return Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'BRL'
   }).format(value)
-
-  return type === 'income' ? formattedValue : `- ${formattedValue}`
 }
 
 export default formatValue
